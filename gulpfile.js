112c734ceada7b291c5140319f62875568614e1f
Zeegster <zaharchi@gmail.com>
2024-05-17 11:26:58 +0200

    add post-css autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,76 @@
-const gulp = require('gulp'),
- webpack = require('webpack'),
- webpackStream = require('webpack-stream'),
- { VueLoaderPlugin } = require('vue-loader'),
- clean = require('gulp-clean'),
- fileInclude = require('gulp-file-include'),
- plumber = require('gulp-plumber'),
- notify = require('gulp-notify'),
- fs = require('fs'),
- browserSync = require('browser-sync').create(),
- sass = require('gulp-sass')(require('sass')),
- sassGlob = require('gulp-sass-glob'),
- sourceMaps = require('gulp-sourcemaps');
+// Importing necessary modules
+import gulp from 'gulp';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
+import { VueLoaderPlugin } from 'vue-loader';
+import fileInclude from 'gulp-file-include';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import fs from 'fs';
+import browserSync from 'browser-sync';
+import sassCompiler from 'sass'; // Importing Dart Sass
+import sassGlob from 'gulp-sass-glob';
+import sourceMaps from 'gulp-sourcemaps';
+import autoPrefixer from 'gulp-autoprefixer';
+import gulpClean from 'gulp-clean';
+import postcss from 'gulp-postcss';
 
+let sass;
+(async () => {
+  const sassModule = await import('gulp-sass');
+  const sassCompilerModule = await import('sass');
+  sass = sassModule.default(sassCompilerModule.default);
+})();
 
-gulp.task('clean:dev', function (done) {
- if (fs.existsSync('./build/')) {
-    return gulp.src('./build/', { read: false }).pipe(clean({ force: true }));
- }
- done();
-});
+// Define tasks using ES module syntax
+export const cleanDev = (done) => {
+  if (fs.existsSync('./build/')) {
+    return gulp.src('./build/', { read: false }).pipe(gulpClean({ force: true }));
+  }
+  done();
+};
 
 const fileIncludeSetting = {
- prefix: '@@',
- basepath: '@file',
+  prefix: '@@',
+  basepath: '@file',
 };
 
-const plumberNotify = (title) => {
- return {
-    errorHandler: notify.onError({
-      title: title,
-      message: 'Error <%= error.message %>',
-      sound: false,
-    }),
- };
-};
-
-gulp.task('html:dev', function () {
- return gulp
-    .src('./src/index.html')
-    .pipe(plumber(plumberNotify('HTML')))
-    .pipe(fileInclude(fileIncludeSetting))
-    .pipe(gulp.dest('./build/'));
+const plumberNotify = (title) => ({
+  errorHandler: notify.onError({
+    title: title,
+    message: 'Error <%= error.message %>',
+    sound: false,
+  }),
 });
 
-gulp.task('sass:dev', function () {
-	return (
-		gulp
-			.src('./src/**/*.scss')
-			.pipe(plumber(plumberNotify('SCSS')))
-			.pipe(sourceMaps.init())
-			.pipe(sassGlob())
-			.pipe(sass())
-			.pipe(sourceMaps.write())
-			.pipe(gulp.dest('./build/css/'))
-	);
-});
+export const htmlDev = () =>
+  gulp
+  .src('./src/index.html')
+  .pipe(plumber(plumberNotify('HTML')))
+  .pipe(fileInclude(fileIncludeSetting))
+  .pipe(gulp.dest('./build/'));
 
-gulp.task('fonts:dev', function () {
-	return gulp
-		.src('./src/fonts/**/*')
-		.pipe(gulp.dest('./build/fonts/'));
-});
+export const sassDev = () =>
+  gulp
+  .src('./src/**/*.scss')
+  .pipe(plumber(plumberNotify('SCSS')))
+  .pipe(sourceMaps.init())
+  .pipe(sassGlob())
+  .pipe(sass())
+  .pipe(postcss())  
+  .pipe(sourceMaps.write())
+  .pipe(gulp.dest('./build/css/'));
 
-gulp.task('vue', function buildVue() {
-  return gulp
-    .src('./src/main.js')
-    .pipe(plumber(plumberNotify('VUE')))
-    .pipe(
+export const fontsDev = () =>
+  gulp
+  .src('./src/fonts/**/*')
+  .pipe(gulp.dest('./build/fonts/'));
+
+export const vueBuild = () =>
+  gulp
+  .src('./src/main.js')
+  .pipe(plumber(plumberNotify('VUE')))
+  .pipe(
       webpackStream({
         mode: 'development',
         module: {
@@ -74,14 +78,17 @@ gulp.task('vue', function buildVue() {
             {
               test: /\.vue$/,
               loader: 'vue-loader',
+              options: 
+                postcss([autoPrefixer()])
+              ,
             },
             {
               test: /\.css$/,
-              use: ['vue-style-loader', 'css-loader'],
+              use: ['vue-style-loader', 'css-loader','postcss-loader'],
             },
             {
               test: /\.scss$/,
-              use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+              use: ['vue-style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             },
           ],
         },
@@ -94,28 +101,28 @@ gulp.task('vue', function buildVue() {
         ],
       })
     )
-    .pipe(gulp.dest('./build/js/'));
-});
+  .pipe(gulp.dest('./build/js/'));
 
-gulp.task('browser-sync', function() {
- browserSync.init({
+export const browserSyncTask = () => {
+  browserSync.init({
     server: {
-      baseDir: "./build"
+      baseDir: './build',
     },
-    notify: false
- });
-});
+    notify: false,
+  });
+};
 
-gulp.task('watch:dev', function () {
- gulp.watch('./src/index.html', gulp.parallel('html:dev'));
- gulp.watch('./src/**/*.scss', gulp.parallel('sass:dev'));
- gulp.watch(['./src/**/*.vue','./src/**/*.js'], gulp.parallel('vue'));
+export const watchDev = () => {
+  gulp.watch('./src/index.html', gulp.parallel(htmlDev));
+  gulp.watch('./src/**/*.scss', gulp.parallel(sassDev));
+  gulp.watch(['./src/**/*.vue', './src/**/*.js'], gulp.parallel(vueBuild));
 
- gulp.watch('./build/**/*').on('change', browserSync.reload);
-});
+  gulp.watch('./build/**/*').on('change', browserSync.reload);
+};
 
-gulp.task(
- 'default',
- gulp.series('clean:dev', gulp.parallel('html:dev','sass:dev', 'vue','fonts:dev'),gulp.parallel('browser-sync', 'watch:dev')),
- 
+// Default task
+export default gulp.series(
+  cleanDev,
+  gulp.parallel(htmlDev, sassDev, vueBuild, fontsDev),
+  gulp.parallel(browserSyncTask, watchDev)
 );
